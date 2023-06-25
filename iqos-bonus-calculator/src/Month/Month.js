99c700ee6094a69daaf1be40fb2a062431bd6d89
damjan <joveski.damjan@uklo.edu.mk>
2023-06-25 23:37:07 +0200

    Month names come dynamically

--- iqos-bonus-calculator/src/Month/Month.js
+++ iqos-bonus-calculator/src/Month/Month.js
@@ -2,9 +2,14 @@ import './Month.css'
 
 function Month(props){
 
+    const months = ['Јануари','Февруари','Март','Април','Мај','Јуни','Јули','Август','Септември','Октромври','Ноември','Декември']
+
+    let today = new Date();
+    let monthIndex = today.getMonth()
+
     return(
         <div className='Month'>
-            <h1>July</h1>
+            <h1>{months[monthIndex]}</h1>
         </div>
     )
 
