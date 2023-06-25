b9503dae3e903c11d70c8956ad98b7ffcf9da0d7
damjan <joveski.damjan@uklo.edu.mk>
2023-06-25 23:19:35 +0200

    Persist on refresh working !

--- iqos-bonus-calculator/src/App.js
+++ iqos-bonus-calculator/src/App.js
@@ -1,19 +1,26 @@
 import './App.css';
 import BonusType from "./bonusType/BonusType";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import TotalSum from "./TotalSum/TotalSum";
+import Month from "./Month/Month";
 
 function App() {
-    const [numberOfAcquisitions, setNumberOfAcquisitions] = useState(0);
-    const [numberOfSwaps, setNumberOfSwaps] = useState(0);
-    const [numberOfSwapsWithFee, setNumberOfSwapsWithFee] = useState(0);
+    const [numberOfAcquisitions, setNumberOfAcquisitions] = useState(
+        !localStorage.getItem('ACQUISITIONS_VALUES') ? 0 : JSON.parse(localStorage.getItem('ACQUISITIONS_VALUES'))
+    );
+    const [numberOfSwaps, setNumberOfSwaps] = useState(
+        !localStorage.getItem('SWAPS_VALUES') ? 0 : JSON.parse(localStorage.getItem('SWAPS_VALUES'))
+    );
+    const [numberOfSwapsWithFee, setNumberOfSwapsWithFee] = useState(
+        !localStorage.getItem('SWAPS_WITH_FEE_VALUES') ? 0 : JSON.parse(localStorage.getItem('SWAPS_WITH_FEE_VALUES'))
+    );
 
     let TotalCount = numberOfAcquisitions + numberOfSwaps + numberOfSwapsWithFee;
 
     const bonusTypes = [
-        { name: "Продажба на прв уред:", count: numberOfAcquisitions,id:1 },
-        { name: "Замени:", count: numberOfSwaps,id:2 },
-        { name: "Замени со доплата:", count: numberOfSwapsWithFee,id:3 }
+        { name: "Продажба на прв уред:", count: numberOfAcquisitions, id: 1 },
+        { name: "Замени:", count: numberOfSwaps, id: 2 },
+        { name: "Замени со доплата:", count: numberOfSwapsWithFee, id: 3 }
     ];
 
     function incrementCounter(bonusTypeId) {
@@ -26,14 +33,40 @@ function App() {
         }
     }
 
+    useEffect(() => {
+        const acquisition = window.localStorage.getItem('ACQUISITIONS_VALUES');
+        const swaps = window.localStorage.getItem('SWAPS_VALUES');
+        const swapsWithFee = window.localStorage.getItem('SWAPS_WITH_FEE_VALUES');
+        setNumberOfAcquisitions(JSON.parse(acquisition) || 0);
+        setNumberOfSwaps(JSON.parse(swaps) || 0);
+        setNumberOfSwapsWithFee(JSON.parse(swapsWithFee) || 0);
+    }, []);
+
+    useEffect(() => {
+        window.localStorage.setItem('ACQUISITIONS_VALUES', JSON.stringify(numberOfAcquisitions));
+    }, [numberOfAcquisitions]);
+
+    useEffect(() => {
+        window.localStorage.setItem('SWAPS_VALUES', JSON.stringify(numberOfSwaps));
+    }, [numberOfSwaps]);
+
+    useEffect(() => {
+        window.localStorage.setItem('SWAPS_WITH_FEE_VALUES', JSON.stringify(numberOfSwapsWithFee));
+    }, [numberOfSwapsWithFee]);
+
     return (
-        <div className='App'>
-            {bonusTypes.map((bonusType) => (
-                <div className='BonusTypes' >
-                    <BonusType id={bonusType.id} name={bonusType.name} count={bonusType.count} incrementCounter={incrementCounter} />
-                </div>
-            ))}
-            <TotalSum totalCount={TotalCount}/>
+        <div>
+            <div className='Month'>
+                <Month />
+            </div>
+            <div className='App'>
+                {bonusTypes.map((bonusType) => (
+                    <div className='BonusTypes' key={bonusType.id}>
+                        <BonusType id={bonusType.id} name={bonusType.name} count={bonusType.count} incrementCounter={incrementCounter} />
+                    </div>
+                ))}
+                <TotalSum totalCount={TotalCount} />
+            </div>
         </div>
     );
 }
---
+++ iqos-bonus-calculator/src/Month/Month.js
@@ -0,0 +1,13 @@
+import './Month.css'
+
+function Month(props){
+
+    return(
+        <div className='Month'>
+            <h1>July</h1>
+        </div>
+    )
+
+}
+
+export default Month;
